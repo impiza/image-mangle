1ea5acb63f2687fd7205a0eaa95533c207a4e833
Matt Hall <bizear@gmail.com>
2013-03-15 21:35:01 +0000

    Minor formatting adjustment

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,12 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
-			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', '!**/libs/*.js'],
+			files: [
+				'Gruntfile.js',
+				'src/**/*.js',
+				'test/**/*.js',
+				'!**/libs/*.js'
+			],
 			options: {
 				globals: {
 					jQuery: true,
