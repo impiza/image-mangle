b63798a6bd326a66c862b54931df2d53453ce079
Matt Hall <bizear@gmail.com>
2013-09-24 22:58:19 +0100

    Updates to Gruntfile to handle connect, concurrent tasks, and compiling and watching Sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,36 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
+		srcDir: './src',
+		devCssDir: '<%= srcDir %>/css',
+		devScssDir: '<%= srcCssDir %>/scss',
+		devJsDir: '<%= srcDir %>/js',
+
 		jshint: {
 			files: [
 				'Gruntfile.js',
-				'src/**/*.js',
-				// 'tests/**/*.js',
-				'!**/libs/*.js'
+				'<%= srcDir %>/**/*.js'
 			],
 			options: {
+				ignores: [
+					'**/libs/*.js',
+					'**/*.min.js',
+					'<%= devJsDir %>/r.js',
+					'<%= devJsDir %>/build.js'
+				],
+				boss: true,
+				browser: true,
+				curly: false,
+				devel: true,
+				eqnull: true,
+				evil: true,
+				immed: false,
+				newcap: false,
+				noarg: true,
+				smarttabs: true,
+				sub: true,
+				undef: true,
 				globals: {
 					console: true,
 					module: true,
@@ -17,10 +39,69 @@ module.exports = function (grunt) {
 					document: true
 				}
 			}
-		}
+		},
+
+		connect: {
+			server: {
+					options: {
+					port: 9001,
+					base: '<%= srcDir %>',
+					keepalive: true 
+				}
+			}
+		},
+
+		sass: {
+			dev: {
+				files: {
+					'<%= devScssDir %>/styles.scss': '<%= srcCssDir %>/styles.css'
+				},
+				options: {
+					style: 'expanded'
+				}
+			},
+			build: {
+				files: {
+					'<%= srcScssDir %>/styles.scss': '<%= srcCssDir %>/styles.css'
+				},
+				options: {
+					style: 'compressed'
+				}
+			}
+		},
+
+		watch: {
+			css: {
+				files: [
+					'<%= devScssDir %>/**/*.scss'
+				],
+				tasks: ['sass:dev']
+			},
+			scripts: {
+				files: [
+					'<%= devJsDir %>/**/*.js',
+					'Gruntfile.js'
+				],
+				tasks: ['jshint']
+			}
+		},
+
+		concurrent: {
+			run: {
+				tasks: ['watch', 'connect'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-concurrent');
 
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('build', ['jshint', 'sass:build']);
+	grunt.registerTask('run', ['concurrent']);
 };
